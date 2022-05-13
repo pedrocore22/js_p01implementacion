ed364d5ccedf20746f184d7dab8a3ef1723a165a
pedrocore22 <pedrocore22@itweblearning.com>
2022-05-13 14:13:10 +0200

    initial

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Sintaxis básica de JavaScript
+/* Comentarios en
+bloque */
+
+// Características principales
+
+// - Ignora los espacios en blanco (a partir del primero)
+// - De tipo case-sensitive (distingue mayúsculas de minúsculas)
+// - Las instrucciones de código finalizan en punto y coma (opcional)
+
+console.log('¡Hola Mundo!');
